eee3d53aef4369acdf14e53f0b3b7832b4107254
ADX2099 <radagasth@gmail.com>
2020-03-17 22:57:27 -0600

    ADD upload Files

---
+++ app/src/main/java/com/adx2099/huaweitest/MainActivity.java
@@ -0,0 +1,14 @@
+package com.adx2099.huaweitest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
