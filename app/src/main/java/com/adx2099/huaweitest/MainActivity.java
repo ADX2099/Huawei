865314e5346703a8e632837e05c2b005855fdc48
ADX2099 <radagasth@gmail.com>
2020-03-17 23:42:28 -0600

    ADD TEST DONE

--- app/src/main/java/com/adx2099/huaweitest/MainActivity.java
+++ app/src/main/java/com/adx2099/huaweitest/MainActivity.java
@@ -3,12 +3,50 @@ package com.adx2099.huaweitest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button calculate;
+    private EditText varA, varB;
+    private TextView TextViewResult;
+    public int total;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setUpView();
+
+
+    }
+
+    private void setUpView() {
+        varA = findViewById(R.id.editTextNumberA);
+        varB = findViewById(R.id.editTextNumberB);
+        TextViewResult = findViewById(R.id.resultText);
+        calculate = findViewById(R.id.button);
+        calculate.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button:
+                sumNumbers();
+                break;
+        }
+    }
+
+    private void sumNumbers() {
+        int a = Integer.parseInt(varA.getText().toString());
+        int b = Integer.parseInt(varB.getText().toString());
+        total = a + b;
+        String theResult = String.valueOf(total);
+        TextViewResult.setText(theResult);
     }
 }
